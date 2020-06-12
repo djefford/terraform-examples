2fc8212f2dc5219958371cc202ef0cd1f8b005f2
djefford <djefford58@gmail.com>
2020-06-11 20:53:36 -0400

    Initial repository commit

---
+++ gcp/example/example.tf
@@ -0,0 +1,34 @@
+provider "google" {
+
+  credentials = file("gcp-terraform.json")
+  project     = "djefford-terraform"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+
+}
+
+resource "google_compute_network" "vpc_network" {
+
+  name = "terraform-network"
+
+}
+
+resource "google_compute_instance" "vm_instance" {
+
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+	tags				 = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+}
