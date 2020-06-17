ebd71aaefec89794a310ada660f29e8f6ef963e3
djefford <djefford58@gmail.com>
2020-06-16 20:00:23 -0400

    Updated file

--- gcp/example/example.tf
+++ gcp/example/example.tf
@@ -17,7 +17,11 @@ resource "google_compute_instance" "vm_instance" {
 
   name         = "terraform-instance"
   machine_type = "f1-micro"
-	tags				 = ["web", "dev"]
+  tags         = ["web", "dev"]
+
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
 
   boot_disk {
     initialize_params {
@@ -28,7 +32,12 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     network = google_compute_network.vpc_network.name
     access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
     }
   }
 
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
 }
